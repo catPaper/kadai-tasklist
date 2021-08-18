e0d72471b93b102d9598f4b73cb7ec0711fbb70a
ryouta.osada <cat.3210sm@gmail.com>
2021-08-19 00:16:01 +0900

    EncodingFilters.javaを作成
    サーブレット内でのrequest/responseのsetCharacterEncoding("UTF-8")
    による文字化け防止の記述を省略するため、フィルタを作成。

---
+++ src/filters/EncodingFilters.java
@@ -0,0 +1,49 @@
+package filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class EncodingFilters
+ */
+@WebFilter("/*")
+public class EncodingFilters implements Filter {
+
+    /**
+     * Default constructor.
+     */
+    public EncodingFilters() {
+    }
+
+    /**
+     * @see Filter#destroy()
+     */
+    public void destroy() {
+    }
+
+    /**
+     * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+     */
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        //サーブレットでの文字化けを防止する
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+
+        // pass the request along the filter chain
+        chain.doFilter(request, response);
+    }
+
+    /**
+     * @see Filter#init(FilterConfig)
+     */
+    public void init(FilterConfig fConfig) throws ServletException {
+    }
+
+}
